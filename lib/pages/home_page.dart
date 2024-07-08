bdbeb3dee0d43c0e796aeea68a3b812e6cae6e8a
panutthon <93002896+panutthon@users.noreply.github.com>
2024-07-08 23:10:27 +0700

    Variables tester

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,61 +11,55 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String fullname = 'AomKung';
+  var products = {
+    'id': 1,
+    'product1': 'Apple',
+    'product2': 'Banana',
+    'product3': 'Orange',
+  };
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      fullname = 'John Doe';
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    ///print('initState');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text('My Flutter test app'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const MyHeader(),
-            const MyHeader(),
-            const MyHeader(),
+            const MyHeader(
+              title: 'Test Header 1',
+            ),
+            const MyHeader(
+              title: 'Header test2',
+            ),
+            const MyHeader(
+              title: 'Header test3',
+            ),
             const MyFooter(),
             const Text(
               'You have pushed the button this many times:',
+            ),
+            Text(
+              fullname,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
             Text(
               '$_counter',
@@ -77,6 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
+        backgroundColor: Colors.red,
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
--- lib/widgets/my_header.dart
+++ lib/widgets/my_header.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class MyHeader extends StatelessWidget {
-  const MyHeader({super.key});
+  final String title;
+  const MyHeader({super.key, required this.title});
+
+  //test widget Text Header required Text title
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello My Header!');
+    return Text(
+      title,
+      style: const TextStyle(color: Colors.green),
+    );
   }
 }
