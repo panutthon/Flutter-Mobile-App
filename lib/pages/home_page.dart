944bc0bd89f7f4cb446724c3a33ef8e635e104c9
panutthon <93002896+panutthon@users.noreply.github.com>
2024-07-12 21:16:39 +0700

    new home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,15 +19,112 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Image.asset('assets/images/logo.png', width: 50, height: 50),
-          centerTitle: true,
+      appBar: AppBar(
+        toolbarHeight: 85.0,
+        // backgroundColor: const Color(0xFF3294FF),
+        // title: Image.asset('assets/images/logo.png', width: 50, height: 50),
+        // centerTitle: true,
+        title: const Text(
+          'WhatAiDan',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
-        body: OutlinedButton(
-          onPressed: () {
-            Get.toNamed('/about');
-          },
-          child: const Text('About Page'),
-        ));
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.calendar_month_outlined,
+                color: Colors.white, size: 30.0),
+            onPressed: () {
+              Get.toNamed('/about');
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.notifications,
+                color: Colors.white, size: 30.0),
+            onPressed: () {
+              Get.toNamed('/about');
+            },
+          ),
+        ],
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF3294FF), Color(0xFF4EC9FF)],
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 5,
+                spreadRadius: 1,
+                offset: Offset(0, 0),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: 350,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(200, 200, 200, 0.5),
+                    blurRadius: 8,
+                    // spreadRadius: 0,
+                    // offset: Offset(0, 4),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 20), // Add some space between the containers
+            Container(
+              height: 100,
+              width: 350,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Color(0xFF73B5FF),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(200, 200, 200, 0.5),
+                    blurRadius: 8,
+                    // spreadRadius: 1,
+                    // offset: Offset(0, 4),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFF3294FF),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.white, size: 40.0),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business, color: Colors.white, size: 40.0),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.white, size: 40.0),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
   }
 }
